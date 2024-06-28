341ee4dbc8903f1c7470a32137062be2dfa69de8
arshivam <shivam.armarkar007@gmail.com>
2024-06-28 22:02:19 +0530

    enchancement in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="bg-secondary-foreground dark:bg-secondary py-6">
       <div className="container mx-auto">
         <div className="flex flex-col items-center justify-between">
-          <Socials containerStyles='flex gap-x-8 mx-auto xl:mx-0 mb-4' iconStyles='text-[20px] text-primary hover:text-white'/>
+          <Socials containerStyles='flex gap-x-8 mx-auto xl:mx-0 mb-4' iconStyles='text-[20px] text-primary dark:text-white/70 hover:text-white dark:hover:text-primary transition-all'/>
           <div className='text-muted-foreground'>
             Copyright &copy; Shivam Armarkar 2024
           </div>
